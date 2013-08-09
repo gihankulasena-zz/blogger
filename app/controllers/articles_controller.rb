fb76d804272664df6ce4883973f7fe1027d57cdc
Gihan <gihan798114@gmail.com>
2013-08-09 22:33:50 +0530

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,19 @@ end
 
 def show
   @article = Article.find(params[:id])
+  @comment = Comment.new
+  @comment.article_id = @article.id
+  
 end
 
 def new
   @article = Article.new
 end
+
+#def comment
+  #@comment = Comment.new
+  #@comment.article_id = @article.id
+#end
 
 def create
   @article = Article.new(article_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Blogger::Application.routes.draw do
   resources :articles
+  resources :comments
   
   root to: 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
